9cd1b1cfe6e9d795264c9bd53286e8577589b233
Erick Germani <erickgermani13@gmail.com>
2023-05-14 13:54:20 -0300

    Tratando exceções nas rotas e adicionando log

--- src/app.js
+++ src/app.js
@@ -1,10 +1,40 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
+const winston = require("winston");
+const uuid = require("uuidv4");
 const router = require("./router");
+
+app.log = winston.createLogger({
+	level: "debug",
+	transports: [
+		new winston.transports.Console({
+			format: winston.format.json({ space: 1 }),
+		}),
+		new winston.transports.File({
+			filename: "logs/error.log",
+			level: "warn",
+			format: winston.format.combine(
+				winston.format.timestamp(),
+				winston.format.json({ space: 1 }),
+			),
+		}),
+	],
+});
 
 app.use("/v1", router);
 
 app.get("/", (req, res) => {
 	res.status(200).send({ message: "Hello World!" });
+});
+
+app.use((err, req, res, next) => {
+	const { name, message, stack } = err;
+
+	const id = uuid();
+	app.log.error({ id, name, message, stack });
+	res.status(500).json({ id, error: "Falha interna" });
+
+	next(err);
 });
 
 module.exports = app;
