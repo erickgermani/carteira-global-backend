d6d0c6270cd16970ff1b7d511fff62ef1b447480
Erick Germani <erickgermani13@gmail.com>
2023-05-14 12:05:28 -0300

    Dividindo o teste da rota /

--- test/app.test.js
+++ test/app.test.js
@@ -1,10 +1,13 @@
 const request = require("supertest");
 const app = require("../src/app");
 
-describe("GET /", () => {
-	test("Deve retornar um JSON com a mensagem Hello World", async () => {
+describe("Ao fazer um GET na rota /", () => {
+	test("Deve retornar o status code 200", async () => {
 		const response = await request(app).get("/");
 		expect(response.status).toBe(200);
+	});
+	test("Deve retornar um JSON com a mensagem Hello World", async () => {
+		const response = await request(app).get("/");
 		expect(response.body).toEqual({ message: "Hello World!" });
 	});
 });
