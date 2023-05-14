2c410520240e327a332722c1c3ea54de46997911
Erick Germani <erick@consultornet.com.br>
2023-05-14 01:08:26 -0300

    Corrigindo o package.json e log do server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,10 @@
 const app = require("./app");
 
-app.listen(3000, () => {
-	console.log("A aplicação está no ar");
+const port = 3000;
+
+app.listen(port, () => {
+	console.log("\n********************************");
+	console.log("A aplicação está rodando localmente!");
+	console.log("Rodando no endereço: http://localhost:" + port);
+	console.log("********************************\n");
 });
